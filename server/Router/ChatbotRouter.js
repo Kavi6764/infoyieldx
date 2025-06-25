7e1db9ce8967d4976169a2b4e2b0060bfe1c275d
Kavi6764 <kavi6764@gmail.com>
2025-06-25 15:05:50 +0530

    edit commit

--- server/Router/ChatbotRouter.js
+++ server/Router/ChatbotRouter.js
@@ -1,29 +1,29 @@
-const express =require("express")
-const openai = require("../Helpers/OpenAi")
-const router =express.Router()
+// const express =require("express")
+// const openai = require("../Helpers/OpenAi")
+// const router =express.Router()
 
-router.post("/messages",async (req,res)=>{
-    const {message} = req.body
+// router.post("/messages",async (req,res)=>{
+//     const {message} = req.body
 
-    const prompt =`You're an AI assistant for a company's HR system.
-- If the user asks how to contact, reply ONLY with: "#go:contact"
-- Otherwise, answer normally.
+//     const prompt =`You're an AI assistant for a company's HR system.
+// - If the user asks how to contact, reply ONLY with: "#go:contact"
+// - Otherwise, answer normally.
 
-User: "${message}"
-`;
+// User: "${message}"
+// `;
 
-try {
-    const result = await openai.chat.completions.create({
-        model: "gpt-4.1",
-      messages: [{ role: "user", content: prompt }],
-    })
+// try {
+//     const result = await openai.chat.completions.create({
+//         model: "gpt-4.1",
+//       messages: [{ role: "user", content: prompt }],
+//     })
     
-const reply = result.choices[0].message.content
-    res.json({ reply });
-  } catch (err) {
-    res.status(500).json({ error: "AI error", details: err.message })
+// const reply = result.choices[0].message.content
+//     res.json({ reply });
+//   } catch (err) {
+//     res.status(500).json({ error: "AI error", details: err.message })
     
-}
-})
+// }
+// })
 
-module.exports =router;
+// module.exports =router;
